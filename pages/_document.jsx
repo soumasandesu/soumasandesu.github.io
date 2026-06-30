b3a9a131fb7cfaed665128cd1cce84825929b492
Charles Poon <hkgsherlock@gmail.com>
2026-07-01 03:53:15 +0800

    ADD gtm

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body className="antialiased">
+        <GoogleTagManager gtmId="GTM-PCWB5MWV" />
         <Main />
         <NextScript />
       </body>
