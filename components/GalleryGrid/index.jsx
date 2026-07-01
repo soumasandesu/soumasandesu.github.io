40eb441ea4a1dcba1a50df3d1a0d3bb15833c0d4
Charles Poon <hkgsherlock@gmail.com>
2026-07-01 15:29:54 +0800

    FIX path join

--- components/GalleryGrid/index.jsx
+++ components/GalleryGrid/index.jsx
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import ImageWithFallback from "./ImageWithFallback";
 
 import styles from "./styles.module.scss";
@@ -8,23 +10,28 @@ export default function GalleryGrid({
 }) {
     return (
         <div className={styles.GalleryGrid}>
-            {images.map((img) => (
-                <a
-                    key={img.alt}
-                    href={`/cos-img/${img.src}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <ImageWithFallback
-                        src={`/cos-img-thumbnails/${img.src}`}
-                        alt={img.alt}
-                        width={img.width}
-                        height={img.height}
-                        unoptimized
-                        fallbackSrc={`/cos-img/${img.src}`}
-                    />
-                </a>
-            ))}
+            {images.map((img) => {
+                const full = path.join('/cos-img', img.src);
+                const thumb = path.join('/cos-img-thumbnails', img.src);
+
+                return (
+                    <a
+                        key={img.alt}
+                        href={full}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <ImageWithFallback
+                            src={thumb}
+                            alt={img.alt}
+                            width={img.width}
+                            height={img.height}
+                            unoptimized
+                            fallbackSrc={full}
+                        />
+                    </a>
+                );
+            })}
 
         </div>
     );
