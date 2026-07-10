e72ecbe1abe68f14dfdee23cd1f7f3998c01fcca
Charles Poon <hkgsherlock@gmail.com>
2026-07-11 05:37:35 +0800

    CHG use yaml to def cos gallery for disp

--- pages/cos_gallery/index.jsx
+++ pages/cos_gallery/index.jsx
@@ -5,6 +5,7 @@ import GalleryGrid from "@/components/GalleryGrid";
 import GallerySection from "@/components/GallerySection";
 
 import styles from "./styles.module.scss";
+import gallerySections from "./defs.yaml";
 
 export default function CosGallery() {
     return (
@@ -13,80 +14,15 @@ export default function CosGallery() {
                 <title>Cos Gallery</title>
             </Head>
 
-            <GallerySection title="2026-01-03 RG33 Zander Netherbrand 2.0" anchorId="rg33-zander-netherbrand-20">
-                <GalleryGrid
-                    images={[
-                        {
-                            src: "/20260103-rg33-awe-zandernetherbrand20/DSC00111a.jpg",
-                            alt: "DSC00111a",
-                            width: 200,
-                            height: 300,
-                        },
-                        {
-                            src: "/20260103-rg33-awe-zandernetherbrand20/DSC00113a.jpg",
-                            alt: "DSC00113a",
-                            width: 200,
-                            height: 300,
-                        },
-                        {
-                            src: "/20260103-rg33-awe-zandernetherbrand20/DSC00115a.jpg",
-                            alt: "DSC00115a",
-                            width: 200,
-                            height: 300,
-                        },
-                        {
-                            src: "/20260103-rg33-awe-zandernetherbrand20/DSC00120a.jpg",
-                            alt: "DSC00120a",
-                            width: 300,
-                            height: 200,
-                        },
-                        {
-                            src: "/20260103-rg33-awe-zandernetherbrand20/IMG_2182a.jpg",
-                            alt: "IMG_2182a",
-                            width: 200,
-                            height: 300,
-                        },
-                        {
-                            src: "/20260103-rg33-awe-zandernetherbrand20/IMG_2184a.jpg",
-                            alt: "IMG_2184a",
-                            width: 300,
-                            height: 200,
-                        },
-                        {
-                            src: "/20260103-rg33-awe-zandernetherbrand20/IMG_2185a.jpg",
-                            alt: "IMG_2185a",
-                            width: 200,
-                            height: 300,
-                        },
-                    ]}
-                />
-            </GallerySection>
-
-            <GallerySection title="2023-07-22 pre-ACGHK2023 Doppio Dropscythe" anchorId="dopi-dropscythe">
-                <GalleryGrid
-                    images={[
-                        {
-                            src: "/20230722-dopi/+PXL_20230722_144802776.jpg",
-                            alt: "DSC00111a",
-                            width: 225,
-                            height: 300,
-                        },
-                        {
-                            src: "/20230722-dopi/+PXL_20230722_145345500.jpg",
-                            alt: "DSC00113a",
-                            width: 225,
-                            height: 300,
-                        },
-                        {
-                            src: "/20230722-dopi/+PXL_20230722_145354458.jpg",
-                            alt: "DSC00115a",
-                            width: 225,
-                            height: 300,
-                        },
-                    ]}
-                />
-            </GallerySection>
-
+            {gallerySections.map(({
+                title,
+                anchorId,
+                images,
+            }) => (
+                <GallerySection key={anchorId} title={title} anchorId={anchorId}>
+                    <GalleryGrid images={images} />
+                </GallerySection>
+            ))}
         </Page>
     );
 }
