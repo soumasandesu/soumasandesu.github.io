4f8a980dbf0cedd359c5388ac37a92f6d656f402
Charles Poon <hkgsherlock@gmail.com>
2026-07-17 04:06:15 +0800

    ADD og meta

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,7 +4,22 @@ import { GoogleTagManager } from '@next/third-parties/google'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="null" />
+        <meta property="og:site_name" content="soumasandesu.github.io" />
+        <meta property="og:url" content="https://soumasandesu.github.io" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        {
+          [{ property: "og:title" }, { name: "twitter:title" }].map(e => <meta key={JSON.stringify(e)} content="Souma's Link-in-bio" {...e} />)
+        }
+        {
+          [{ property: "og:description" }, { name: "twitter:description" }].map(e => <meta key={JSON.stringify(e)} content="Souma's Link-in-bio" {...e} />)
+        }
+        {
+          [{ property: "og:image" }, { name: "twitter:image" }].map(e => <meta key={JSON.stringify(e)} content="/og_cover.png" {...e} />)
+        }
+      </Head>
       <body className="antialiased">
         <GoogleTagManager gtmId="GTM-PCWB5MWV" />
         <Main />
