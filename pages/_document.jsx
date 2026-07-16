703cb55109565624d80f1d9d5ff58a7190e7bc87
Charles Poon <hkgsherlock@gmail.com>
2026-07-17 04:15:16 +0800

    FIX font style

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,6 +5,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;700&amp;Kosugi+Maru&amp;family=M+PLUS+1p:wght@800&amp;family=M+PLUS+Rounded+1c:wght@800&amp;family=Noto+Serif+JP:wght@900&amp;family=Kaisei+Decol:wght@400;700&amp;family=Potta+One&amp;family=Yuji+Mai&amp;family=Hachi+Maru+Pop&amp;display=swap" />
+
         <meta name="description" content="null" />
         <meta property="og:site_name" content="soumasandesu.github.io" />
         <meta property="og:url" content="https://soumasandesu.github.io" />
